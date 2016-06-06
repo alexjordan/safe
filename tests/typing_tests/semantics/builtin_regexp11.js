c837bd19034716bf35ba3c781fffcfa3173d9633
Alexander Jordan <alexander.jordan@oracle.com>
2016-06-06 23:12:03 +1000

    Extends RegExp test
    Adds a simple sanity test for the RegExp constructor.

--- tests/typing_tests/semantics/builtin_regexp11.js
+++ tests/typing_tests/semantics/builtin_regexp11.js
@@ -9,7 +9,9 @@
 
 var o_1 = new RegExp("^(?:\\s*(<[\\w\\W]+>)[^>]*|#([\\w-]*))$");
 var o_2 = RegExp(o_1, undefined);
+var o_3 = new RegExp(o_1, undefined)
 
 var __result1 = o_1 == o_2;
 var __expect1 = true;
-
+var __result2 = o_1 == o_3;
+var __expect2 = false;
