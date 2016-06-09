a176e772bb748d735e0b3b25a47c57d66efc0c64
Alexander Jordan <alexander.jordan@oracle.com>
2016-06-09 19:54:54 +1000

    Adds concrete ECMA tests for Array.splice()

--- src/test/scala/ConcreteTestRunner.scala
+++ src/test/scala/ConcreteTestRunner.scala
@@ -10,17 +10,34 @@ import kr.ac.kaist.jsaf.{Shell, ShellParameters}
 class ConcreteTestSuite extends TestCase("Concrete") {}
 
 object ConcreteTestSuite {
-  // TODO only one directory has tests so far
-  val TESTS_DIR = "tests/concrete_semantics/ecma/GlobalObject"
+  val TESTS_DIR = "tests/concrete_semantics/ecma"
 
   Shell.pred = new Predefined(new ShellParameters())
 
   def suite: Test = {
     val suite = new TestSuite("ConcreteSuite")
 
-    // TODO simplistic filter for .js files
-    for (filename <- Option(new File(TESTS_DIR).list).getOrElse(Array[String]()).filter(s => s.endsWith(".js")))
-      suite.addTest(new ConcreteECMASemantics(TESTS_DIR, filename))
+    def collect(path: File): List[File] = {
+      var l = List[File]()
+      // TODO could apply filter here
+      Option(path.listFiles()) match {
+        case Some(files) =>
+          files.foreach(f =>
+            if (f.isDirectory)
+              l ++= collect(f)
+            else
+              l :+= f
+          )
+        case None => ()
+      }
+      l
+    }
+
+
+    // TODO filter for files is .js extension
+    for (file <- collect(new File(TESTS_DIR)).filter(f => f.getName.endsWith(".js")))
+      suite.addTest(new ConcreteECMASemantics(file))
     suite
+
   }
 }
